baa5475c7fe04c5feabc464dfb11a0e1d8cc5ac4
Facu <faaatorres@hotmail.com>
2021-10-26 17:15:39 -0300

    a

--- Front/PantallasFacu/perfilUsuario.php
+++ Front/PantallasFacu/perfilUsuario.php
@@ -1,20 +1,11 @@
 
-
-<head>
-  
-  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-
 <?php
 require "head.php";
 require "header.php";
 ?>  
 
-<hr>
-<div class="container bootstrap snippet">
+
+<div class="container">
     <div class="row">
   		<div class="col-sm-10"><h1> <label id="lblUser"></label> </h1></div>
     	<div class="col-sm-2"><a href="/users" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="../imagenes/logo.jpg"></a></div>
@@ -23,14 +14,14 @@ require "header.php";
   		<div class="col-sm-3"><!--left col-->
               
 
-      <div class="text-center">
+      <div class="container">
         <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" id="foto" class="avatar img-circle img-thumbnail" alt="avatar">
         <input type="file" id="archivo" class="text-center center-block file-upload">
         <button  id="btnSubirFoto"  type="submit"> Subir foto</button>
       </div></hr><br>
 
                
-          <div class="panel panel-default">
+          <div class="container">
             <div class="panel-heading">Website <i class="fa fa-link fa-1x"></i></div>
             <div class="panel-body"><a href="http://bootnipets.com">swaparea.com</a></div>
           </div>
@@ -52,51 +43,47 @@ require "header.php";
           </div>
           
         </div><!--/col-3-->
-    	<div class="col-sm-9">
-            <ul class="nav nav-tabs">
+    	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            
                 <li class="active"><a data-toggle="tab" href="#home">Datos personales</a></li>
                 
-                
-              </ul>
-
-              
-          
-            <div class="tab-pane active" id="home">
-                <hr>
+            
+            
+                <div class="card-body">
                   <form class="form" action="##" method="post" id="registrationForm">
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="first_name"><h4>Nombres</h4></label>
-                              <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
+                          <div class="col-md-12">
+                            <label for="name">Nombres</label>
+                              <input type="text" class="form-control" name="name" id="name" placeholder="name" title="enter your name if any.">
                           </div>
                       </div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                            <label for="last_name"><h4>Apellidos</h4></label>
+                          <div class="col-md-12">
+                            <label for="last_name">Apellidos</label>
                               <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
                           </div>
                       </div>
         
           
                       <div class="form-group">
-                          <div class="col-xs-6">
-                             <label for="mobile"><h4>telefono</h4></label>
+                          <div class="col-md-12">
+                             <label for="mobile">Telefono</label>
                               <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
                           </div>
                       </div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Email</h4></label>
+                          <div class="col-md-12">
+                              <label for="email">Email</label>
                               <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
                           </div>
                       </div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Edad</h4></label>
+                          <div class="col-md-12">
+                              <label for="email">Edad</label>
                               <input  class="form-control" id="edad" placeholder="Edad" title="enter a location">
                           </div>
                       </div>
@@ -106,66 +93,53 @@ require "header.php";
                       <div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="password"><h4>Password Actual</h4></label>
+                          <div class="col-md-12">
+                              <label for="password">Password Actual</label>
                               <input type="password" class="form-control" name="password" id="passwordold" placeholder="password" title="enter your password.">
                           </div>
                       </div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="password"><h4>Password nueva</h4></label>
+                          <div class="col-md-12">
+                              <label for="password">Password nueva</label>
                               <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
                           </div>
                       </div>
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                            <label for="password2"><h4>Verify</h4></label>
+                          <div class="col-md-12">
+                            <label for="password2">Verify</label>
                               <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
                           </div>
                       </div>
                       
                       <div class="form-group">
-                           <div class="col-xs-12">
+                           <div class="col-md-12">
                                 <br>
                       <button class="btn btn-lg btn-success" id="btnpass"  type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Cambiar Contraseña</button>
                    
                      </div>
                     </div>
-
-
-                 	</form>
-              
-              <hr>
-              
-             </div><!--/tab-pane-->
-             
-               
-               <h2></h2>
-               
-               <hr>
-                 
-                      <div class="form-group">
+                    <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="first_name"><h4>Calle y numero</h4></label>
+                          <div class="col-md-12">
+                              <label for="first_name">Calle y numero></label>
                               <input type="text" class="form-control" name="first_name" id="dimicilio" placeholder="Calle" title="enter your first name if any.">
                           </div>
                       </div>
           
                       <div class="form-group">
                           
-                          <div class="col-xs-6">
-                              <label for="phone"><h4>Ciudad</h4></label>
+                          <div class="col-md-12">
+                              <label for="phone">Ciudad</label>
                               <select class="custom-select form-control" id="departamento">
                               </select>
                           </div>
                       </div>
           
                       <div class="form-group">
-                          <div class="col-xs-6">
-                             <label for="mobile"><h4>Provincia</h4></label>
+                          <div class="col-md-12">
+                             <label for="mobile">Provincia</label>
                              <select class="custom-select form-control" id="provincia">
                              </select>
                     </select>
@@ -180,18 +154,27 @@ require "header.php";
                           
                       </div>
                       <div class="form-group">
-                           <div class="col-xs-12">
+                           <div class="col-md-12">
                                 <br>
-                              	<button class="btn btn-lg btn-success" id="guardarDomiciclio" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
-                               	
+                                <button class="btn btn-lg btn-success" id="guardarDomiciclio" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
+                                
                             </div>
                       </div>
+
+                 	</form>
+              
+              <hr>
+              
+             </div><!--/tab-pane-->
+             
+                 
+                      
               	
-               
+  </div>             
              
            
     <?php 
-//require "footer.php";
- ?>    
+    require "footer.php";
+    ?>    
  
  <script src="../../js/UsuarioPerfil.js"></script>
